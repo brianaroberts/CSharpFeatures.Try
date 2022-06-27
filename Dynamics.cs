17ebb347d7eace954b923155f676fb063e52e7bb
Brian A Roberts <brianaroberts@live.com>
2022-06-27 09:42:54 -0400

    Inital

---
+++ Dynamics.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Utilities;
+
+namespace CSharpLanguageFeatures.Try
+{
+    public class Dynamics
+    {
+        UserInputCallbacks _caller; 
+
+        public Dynamics(UserInputCallbacks userInputCallbacks)
+        {
+            _caller = userInputCallbacks;
+        }
+
+        public void AnimalsThatSpeak(string Animal)
+        {
+            string animalType; 
+
+            switch (Animal.ToLower())
+            {
+                case "dog":
+                    animalType = "Dog";
+                    break; 
+                default:
+                    animalType = "Animal";
+                    break; 
+            }
+
+            try
+            {
+                Type dynamicAnimalType = Assembly.Load("Animals").GetType(animalType); 
+                dynamic animal = Activator.CreateInstance(dynamicAnimalType);
+                animal.Speak();
+            }
+            catch (Exception e)
+            {
+                // TODO: Base in user interface
+                _caller.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLanguageFeatures.Try
+{    
+    public static class FilterExtensions
+    {
+        public static IEnumerable<T> Filter<T> (this IEnumerable<T> input, FilterDelegate<T> predicate)
+        {
+            foreach(var item in input)
+            {
+                if (predicate(item))
+                {
+                    yield return item; 
+                }
+            }
+        }
+        public delegate bool FilterDelegate<T>(T item);
+    }       
+}
